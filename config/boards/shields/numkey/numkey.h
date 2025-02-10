e378bfc2a30591ef4f8d99c0c9431c7b4e9b8133
Finnitio <f.bosholm@gmail.com>
2025-02-10 14:56:57 +0100

    fix numkey.h

--- config/boards/shields/numkey/numkey.h
+++ config/boards/shields/numkey/numkey.h
@@ -5,7 +5,7 @@
   │ 12  13  14  15  16  17 │ 18  19  20  21  22  23 │ │ LM5 LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 RM5 │
   │ 24  25  26  27  28  29 │ 30  31  32  33  34  35 │ │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
   │                              36  37  38         │ │                         │     RN1 RN2 RN3         │
-  ╰───────────╮ 39  40  41 │ 41  42  43 ╭───────────╯ ╰───────────╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────────╯
+  ╰───────────╮ 39  40  41 │ 42  43  44 ╭───────────╯ ╰───────────╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────────╯
               ╰────────────┴────────────╯                         ╰─────────────┴─────────────╯             */
 
 #pragma once
@@ -52,13 +52,13 @@
 #define RB4 34
 #define RB5 35
 
-#define RN1 37  // right thumb keys
-#define RN2 38
-#define RN3 39
+#define RN1 36  // right thumb keys
+#define RN2 37
+#define RN3 38
 
-#define LH0 39  // left thumb keys
+#define LH0 41  // left thumb keys
 #define LH1 40
-#define LH2 41
+#define LH2 39
 
 #define RH0 42  // right thumb keys
 #define RH1 43
